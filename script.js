027306ccfd11859482102694b88dfa414b5bcf41
albaV3857 <96639439+albaV3857@users.noreply.github.com>
2022-03-27 15:51:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+name_of_the_book_array=[];
+
+function submit()
+{
+    var name_1 = document.getElementById("name_of_the_book_1").value;
+    var name_2 = document.getElementById("name_of_the_book_2").value;
+    var name_3 = document.getElementById("name_of_the_book_3").value;
+    var name_4 = document.getElementById("name_of_the_book_4").value;
+    
+    name_of_the_book_array.push(name_1);
+    name_of_the_book_array.push(name_2);
+    name_of_the_book_array.push(name_3);
+    name_of_the_book_array.push(name_4);
+
+    console.log(name_of_the_book_array);
+
+    document.getElementById("display_name").innerHTML = name_of_the_book_array;
+    document.getElementById("submit_button").style.display = "none";
+    document.getElementById("sort_button").style.display = "inline-block";
+}
+
+function sorting()
+{
+    name_of_the_book_array.sort();
+    console.log(name_of_the_book_array);
+    document.getElementById("display_name").innerHTML = name_of_the_book_array;
+}
